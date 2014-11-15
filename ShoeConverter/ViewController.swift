15d37b7cf185788a009597af52f73ffac2f7b56e
Karl Harrison <karl_harrison@icloud.com>
2014-11-14 21:17:06 -0500

    First Commit

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensTextField: UITextField!
+    @IBOutlet weak var mensConvertedLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+
+        let integerFromTextField = mensTextField.text.toInt()!
+        let conversionConstant = 30
+        
+        mensConvertedLabel.hidden = false
+        mensConvertedLabel.text = "\(integerFromTextField + conversionConstant)" + " in European Shoes Size"
+    }
 
 }
 
